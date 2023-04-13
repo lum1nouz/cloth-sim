e7df964d739db8bf486e8d06a09f13cc89f75f5d
Cliff Xu <xu.cliffjun@gmail.com>
2023-04-13 17:07:31 -0500

    initial mesh implementation

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var clothGeometry;
+
+// Tutorial geometry
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
@@ -17,10 +20,33 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
 }
+
+function plane(width, height) {
+
+	return function(u, v) {
+
+		var x = (u - 0.5) * width;
+		var y = (v + 0.5) * height;
+
+		return new THREE.Vector3( x, y, 0 );
+
+	};
+
+}
+
+function init() {
+    // TODO: Make sure to change the top variables so that they are initialized here
+    //       instead
+    // TODO: Custom parameters to match what we want
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    clothGeometry = new THREE.ParametricGeometry()
+    clothGeometry.dynamic = true;
+}
+
+
 animate();
 
